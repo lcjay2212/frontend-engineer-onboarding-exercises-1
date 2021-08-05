2e7ea1a1a5baedef9674423f0a4c0c2e1feea85f
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-08-05 11:39:18 +0800

    feat: create product details page

---
+++ pages/[id].tsx
@@ -0,0 +1,8 @@
+import { Box } from '@chakra-ui/react';
+import { FC } from 'react';
+
+const ProductDetails: FC = () => {
+  return <Box h="52rem">Products Details</Box>;
+};
+
+export default ProductDetails;
