ee45a22a2cefa009752e752a44e6563584bc22a7
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-08-05 11:39:57 +0800

    feat: Added mock data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Box, ChakraProvider } from '@chakra-ui/react';
+import Footer from '@components/Footer';
 import Navbar from '@components/Navbar';
 import { AppProps } from 'next/app';
 import { FC } from 'react';
@@ -6,9 +7,10 @@ import { FC } from 'react';
 const App: FC<AppProps> = ({ Component, pageProps }) => (
   <ChakraProvider>
     <Navbar />
-    <Box w="100%" bg="#F7FAFC" borderTop="1px" borderColor="gray.300">
-      <Box maxh={'100vh'} margin="auto" maxW="90rem">
+    <Box bg="#F7FAFC" borderTop="1px" borderColor="gray.300">
+      <Box margin="auto" maxW="90rem" h="52rem">
         <Component {...pageProps} />
+        <Footer />
       </Box>
     </Box>
   </ChakraProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,79 @@
-import { Box, Grid, Text } from '@chakra-ui/react';
-import Footer from '@components/Footer';
-import NextImage from 'next/image';
+import { Box } from '@chakra-ui/react';
+import List from 'components/ProductLists';
 import { FC } from 'react';
 
-const Home: FC = () => (
-  <Box>
-    <Grid placeContent="center" h="100vh">
-      <NextImage src="/logo.png" width={200} height={200} />
-      <Text fontSize="xl" mt="2rem" textAlign="center">
-        Welcome to HOV!
-      </Text>
-    </Grid>
-    <Footer />
-  </Box>
-);
+export interface ProductDataProps {
+  id: string;
+  image: string;
+  title: string;
+  content: string;
+}
+
+const Home: FC = () => {
+  const data: ProductDataProps[] = [
+    {
+      id: '1',
+      image: 'https://images.pond5.com/professional-it-programer-working-data-footage-103271395_iconl.jpeg',
+      title: 'ReactJS',
+      content:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      id: '2',
+      image: 'https://images.pond5.com/professional-it-programer-working-data-footage-103271395_iconl.jpeg',
+      title: 'ReactJS',
+      content:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      id: '3',
+      image: 'https://images.pond5.com/professional-it-programer-working-data-footage-103271395_iconl.jpeg',
+      title: 'ReactJS',
+      content:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      id: '4',
+      image: 'https://images.pond5.com/professional-it-programer-working-data-footage-103271395_iconl.jpeg',
+      title: 'ReactJS',
+      content:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      id: '5',
+      image: 'https://images.pond5.com/professional-it-programer-working-data-footage-103271395_iconl.jpeg',
+      title: 'ReactJS',
+      content:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      id: '6',
+      image: 'https://images.pond5.com/professional-it-programer-working-data-footage-103271395_iconl.jpeg',
+      title: 'ReactJS',
+      content:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      id: '7',
+      image: 'https://images.pond5.com/professional-it-programer-working-data-footage-103271395_iconl.jpeg',
+      title: 'ReactJS',
+      content:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      id: '8',
+      image: 'https://images.pond5.com/professional-it-programer-working-data-footage-103271395_iconl.jpeg',
+      title: 'ReactJS',
+      content:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+  ];
+  return (
+    <Box>
+      <List data={data} />
+      {/* <Footer /> */}
+    </Box>
+  );
+};
 
 export default Home;
