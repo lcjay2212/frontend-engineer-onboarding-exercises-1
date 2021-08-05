68aa8574a06d75ed44eac62428c9a63c7d1e6655
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-08-05 15:57:17 +0800

    feat: implement that footer is not visible on signup and login form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,21 @@ import { Box, ChakraProvider } from '@chakra-ui/react';
 import Footer from '@components/Footer';
 import Navbar from '@components/Navbar';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/dist/client/router';
 import { FC } from 'react';
 
-const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <ChakraProvider>
-    <Navbar />
-    <Box bg="#F7FAFC" borderTop="1px" borderColor="gray.300">
-      <Box margin="auto" maxW="90rem" h="100%" minH="52rem">
-        <Component {...pageProps} />
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+  return (
+    <ChakraProvider>
+      <Navbar />
+      <Box bg="#F7FAFC" borderTop="1px" borderColor="gray.300">
+        <Box margin="auto" maxW="90rem" h="100%" minH="52rem">
+          <Component {...pageProps} />
+        </Box>
+        {!(router.pathname === '/login' || router.pathname === '/signup') && <Footer />}
       </Box>
-      <Footer />
-    </Box>
-  </ChakraProvider>
-);
-
+    </ChakraProvider>
+  );
+};
 export default App;
