410163024b6a3218e01b1b7b534e25a88816e1c3
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-08-06 16:48:42 +0800

    feat: adjust styled

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,9 +4,31 @@ import { SocialMediaLinks } from './SocialMediaLinks';
 
 const Footer: FC = () => {
   return (
-    <Box as="footer" role="contentinfo" mx="auto" px={112} h="4rem" bg="white" maxW="90rem">
-      <Stack direction="row" spacing="4" align="center" justify="space-between" h="4rem">
-        <Text fontSize="1rem" fontWeight={400} color="#9CA3AF" lineHeight="1.5rem">
+    // <Box as="footer" role="contentinfo" mx="auto" px={112} h="4rem" bg="white" maxW="90rem">
+    //   <Stack direction="row" spacing="4" align="center" justify="space-between" h="4rem">
+    //     <Text fontSize="1rem" fontWeight={400} color="#9CA3AF" lineHeight="1.5rem">
+    //       &copy; {new Date().getFullYear()} HOV Onboarding. All rights reserved.
+    //     </Text>
+    //     <SocialMediaLinks />
+    //   </Stack>
+    // </Box>
+    <Box
+      as="footer"
+      role="contentinfo"
+      mx="auto"
+      px="7rem"
+      h="4rem"
+      bg="white"
+      maxW="90rem"
+      py={{ base: '1.15rem', lg: 0 }}
+    >
+      <Stack
+        h="4rem"
+        direction={{ base: 'column-reverse', md: 'column-reverse', lg: 'row' }}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Text fontSize="1rem" fontWeight={400} color="#9CA3AF" lineHeight="1.5rem" mb={{ base: '.50rem', lg: 0 }}>
           &copy; {new Date().getFullYear()} HOV Onboarding. All rights reserved.
         </Text>
         <SocialMediaLinks />
