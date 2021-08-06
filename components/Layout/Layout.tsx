2384af60529a2b092c27ada80a9a0f4fdb509e18
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-08-06 11:05:37 +0800

    feat: adjust some style

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@chakra-ui/react';
+import Footer from '@components/Footer/Footer';
+import Navbar from '@components/Navbar/Navbar';
+import { useRouter } from 'next/dist/client/router';
+import { FC } from 'react';
+
+const Layout: FC = ({ children }) => {
+  const router = useRouter();
+  return (
+    <>
+      <Navbar />
+      <Box bg="#F7FAFC" borderTop="1px" borderColor="gray.300">
+        <Box margin="auto" maxW="90rem" h="100%" minH="52rem">
+          {children}
+        </Box>
+        {!(router.pathname === '/login' || router.pathname === '/signup') && <Footer />}
+      </Box>
+    </>
+  );
+};
+
+export default Layout;
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -10,81 +10,67 @@ import {
   MenuItem,
   MenuList,
   Stack,
-  Tab as ChackraTab,
-  Tabs,
   Text,
 } from '@chakra-ui/react';
 import useUser, { UserLogInProps } from 'hooks/useUser';
+import { useRouter } from 'next/dist/client/router';
 import { FC } from 'react';
 import { HiOutlineBell } from 'react-icons/hi';
+import Logo from './Logo';
 import MenuLink from './MenuLinks';
 
-const Tab: FC = ({ children }) => (
-  <ChackraTab
-    _selected={{
-      px: 0,
-      borderColor: '#6366F1',
-      h: '3rem',
-      ml: '1.780625rem',
-      borderBottom: 'solid #6366F1',
-    }}
-  >
-    {children}
-  </ChackraTab>
-);
+const buttonStyle = {
+  fontSize: '1rem',
+  lineHeight: '1.5rem',
+  fontWeight: 600,
+};
+
+const menuProfile = {
+  borderRadius: '50%',
+  height: '2rem',
+  width: '2rem',
+};
 
 const Navbar: FC = () => {
   const { isLoggedIn } = useUser((state: UserLogInProps) => ({
     isLoggedIn: state.isLoggedIn,
   }));
+  const router = useRouter();
+  const path = router.pathname === '/';
 
   return (
     <Box px={112} h="4rem" maxW="90rem" margin="auto">
       <Flex h="4rem" alignSelf="center">
         <Flex py="17px" flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          <Image
-            d="flex"
-            alt="cover-image"
-            justifyContent="flex-start"
-            boxSize="1.875rem"
-            objectFit="cover"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-          />
-          <Text fontSize={22} fontWeight={'bold'} color="black" ml="0.625rem">
-            workflow
+          <Logo />
+          <Text
+            alignSelf="center"
+            h="4rem"
+            fontSize={14}
+            fontWeight={'500'}
+            color="#6B7280"
+            ml="1.780625rem"
+            pt="1.4375rem"
+            borderBottom={path ? 'solid #6366F1' : ''}
+          >
+            <MenuLink title="Products" />
           </Text>
-
-          <Tabs variant="unstyled">
-            <Tab>
-              <Text h="1.75rem" alignSelf="center" fontSize={14} fontWeight={'500'} color="#6B7280">
-                <MenuLink title="Products" />
-              </Text>
-            </Tab>
-          </Tabs>
         </Flex>
         {isLoggedIn ? (
           <>
             <Stack flex={{ base: 1, md: 0 }} justify={'flex-end'} direction={'row'} spacing={4} alignSelf="center">
-              <Button fontSize="1rem" lineHeight="1.5rem" fontWeight={600}>
+              <Button style={buttonStyle}>
                 <MenuLink title="Log in" />
               </Button>
-              <Button
-                display={{ base: 'none', md: 'inline-flex' }}
-                fontSize={'sm'}
-                color={'white'}
-                colorScheme="purple"
-                bg="#805AD5"
-                lineHeight="1.5rem"
-              >
+              <Button style={buttonStyle} color="white" colorScheme="purple" bg="#805AD5">
                 <MenuLink title="Sign up" />
               </Button>
             </Stack>
           </>
         ) : (
-          <>
+          <Stack spacing={3} direction="row">
             <IconButton
               alignSelf="center"
-              pr="1rem"
               as="a"
               href="#"
               aria-label="notification"
@@ -94,20 +80,10 @@ const Navbar: FC = () => {
               icon={<HiOutlineBell fontSize="1.25rem" />}
             />
             <Menu>
-              <MenuButton
-                alignSelf="center"
-                as={IconButton}
-                borderRadius="full"
-                h="2rem"
-                w="2rem"
-                bg="white"
-                colorScheme="white"
-              >
+              <MenuButton alignSelf="center" as={IconButton} style={menuProfile} colorScheme="white">
                 <Image
                   alt="profile"
-                  borderRadius="full"
-                  h="32px"
-                  w="32px"
+                  style={menuProfile}
                   src="https://en.kepoper.com/wp-content/uploads/2021/01/twice-mina-profile-1-wm.jpg"
                 />
               </MenuButton>
@@ -118,7 +94,7 @@ const Navbar: FC = () => {
                 </MenuGroup>
               </MenuList>
             </Menu>
-          </>
+          </Stack>
         )}
       </Flex>
     </Box>
