a552a92c9cfe0e04e007f6291694fe91658a684b
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-08-06 14:02:38 +0800

    feat: adjust footer place

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -7,15 +7,15 @@ import { FC } from 'react';
 const Layout: FC = ({ children }) => {
   const router = useRouter();
   return (
-    <>
+    <Box>
       <Navbar />
       <Box bg="#F7FAFC" borderTop="1px" borderColor="gray.300">
         <Box margin="auto" maxW="90rem" h="100%" w="100%" minH="52rem">
           {children}
         </Box>
-        {!(router.pathname === '/login' || router.pathname === '/signup') && <Footer />}
       </Box>
-    </>
+      {!(router.pathname === '/login' || router.pathname === '/signup') && <Footer />}
+    </Box>
   );
 };
 
