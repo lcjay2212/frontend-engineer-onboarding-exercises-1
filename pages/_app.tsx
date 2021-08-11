88914febc621369131786735f4fc4d7f039de980
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-08-11 12:51:59 +0800

    feat: use redux as a Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,21 @@
 import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '@components/Layout';
+import { store } from '@store/store';
 import { AppProps } from 'next/app';
 import { client } from 'queries/client';
 import { FC } from 'react';
+import { Provider } from 'react-redux';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ApolloProvider client={client}>
       <ChakraProvider>
-        {/* <Provider store={store}> */}
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-        {/* </Provider> */}
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
       </ChakraProvider>
     </ApolloProvider>
   );
