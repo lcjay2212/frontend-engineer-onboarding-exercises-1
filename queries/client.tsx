e6330e9efb36b991d001bf178524b6642afcd5d8
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-08-11 19:08:15 +0800

    feat: set headers authorization on client

--- queries/client.tsx
+++ queries/client.tsx
@@ -1,6 +1,23 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'https://frontend-engineer-onboarding-api-thxaa.ondigitalocean.app/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 export const client = new ApolloClient({
-  uri: `https://frontend-engineer-onboarding-api-thxaa.ondigitalocean.app/graphql`,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
